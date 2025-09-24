50e6d8f64b1fb520b16d04d1c43592692f1226b5
Willmch161 <kliam7395@gmail.com>
2025-09-24 14:56:17 +0700

    first commit

---
+++ PredictTheValue.js
@@ -0,0 +1,4 @@
+let a2 = 5,
+    b2 = 2;
+let result = a2 + b2 * a2;
+console.log(`4. Result = ${result} (karena 5 + (2*5))`);
---
+++ SwapTheValue.js
@@ -0,0 +1,4 @@
+let x = 10,
+    y = 20;
+[x, y] = [y, x];
+console.log("1. Swap -> x =", x, ", y =", y);
